a5fbf5815cf73651b18e98a0e6648ed85c8dac9c
Jaay7 <jayakrishna.madem@gmail.com>
2022-01-15 22:54:53 +0530

    home page

---
+++ src/data.js
@@ -0,0 +1,50 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default 
+  {  
+    landingPages: [
+      {
+        "id": "01",
+        "title": "First",
+        "description": "This is the first landing page",
+        "link": "/first",
+      },
+      {
+        "id": "02",
+        "title": "Second",
+        "description": "This is the second landing page",
+        "link": "/second",
+      },
+      {
+        "id": "03",
+        "title": "Third",
+        "description": "This is the third landing page",
+        "link": "/third",
+      },
+      {
+        "id": "04",
+        "title": "Fourth",
+        "description": "This is the fourth landing page",
+        "link": "/fourth",
+      },
+      {
+        "id": "05",
+        "title": "Fifth",
+        "description": "This is the fifth landing page",
+        "link": "/fifth",
+      },
+      {
+        "id": "06",
+        "title": "Sixth",
+        "description": "This is the sixth landing page",
+        "link": "/sixth",
+      }
+    ],
+    dashboardPages: [
+      {
+        "id": "1",
+        "title": "First",
+        "description": "This is the first dashboard page",
+        "link": "/dashboard/first",
+      }
+    ]
+  }
